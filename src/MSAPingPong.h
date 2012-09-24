3d7b2dfdfffcd1f2c57a1a0cbb28e417d5a59e2d
Memo Akten <memo@memo.tv>
2012-09-24 04:28:05 +0100

    first commit

---
+++ src/MSAPingPong.h
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace msa {
+	
+	template<class T>
+	class PingPong {
+	public:
+		
+		PingPong() {
+			currentIndex = 0;
+		}
+		
+		T& getFront() {
+			return objects[currentIndex];
+		}
+		
+		T& getBack() {
+			return objects[1-currentIndex];
+		}
+		
+		void swap() {
+			currentIndex = 1 - currentIndex;
+		}
+		
+	protected:
+		T		objects[2];
+		int		currentIndex;
+	};
+}
