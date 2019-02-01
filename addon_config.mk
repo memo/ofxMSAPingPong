35ac36abc529671d819c94d22d7f2f44b97829c9
Memo Akten <memo@memo.tv>
2019-02-01 04:52:13 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSAPingPong
+	ADDON_DESCRIPTION = Templated class to ping pong 
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/ofxMSAPingPong
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES =
